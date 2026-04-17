1d1ba3b35628fa15a052a8317572c395c0f18495
Bifani <gbifani.tech@gmail.com>
2026-04-17 18:03:45 -0300

    fix: adding subrequest dto

---
+++ src/main/java/br/com/bifani/subscriptions_java_spring/Entities/DTOs/SubscriptionRequest.java
@@ -0,0 +1,8 @@
+package br.com.bifani.subscriptions_java_spring.Entities.DTOs;
+
+import br.com.bifani.subscriptions_java_spring.Entities.Enums.SubscriptionEnum;
+
+import java.math.BigDecimal;
+
+public record SubscriptionRequest(SubscriptionEnum subscriptionType,
+                                  BigDecimal price) {}
